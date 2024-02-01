013e0f35738987d8ce185c10286e082ecc913bd3
Sudharsan <126713117+Sidreyas@users.noreply.github.com>
2024-02-01 22:50:09 +0530

    Update hyper.py

--- hyper.py
+++ hyper.py
@@ -86,7 +86,6 @@ def user_input(user_question):
 
 
 def main():
-    st.set_page_config("Chat PDF")
     st.header("Chat with PDF using Gemini")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
